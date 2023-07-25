5dccc6f7e723cb81f6d60907513c1bb1a28aeac0
SUBBAREDDYAYYALA <subbareddya2022@gmail.com>
2023-07-25 12:08:04 +0530

    first commit

---
+++ app2/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from app2.views import *
+
+name='app2'
+
+urlpatterns=[
+    path('html3/',html3,name='html3'),
+    path('html4/',html4,name='html4')
+]
---
+++ app2/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def html3(request):
+    return render(request,'html3.html')
+def html4(request):
+    return render(request,'html4.html')
